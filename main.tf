def846cf8e45e23725f28f5fe3bb37e1a71d1c4c
magamudi <118616073+magamudi@users.noreply.github.com>
2023-03-27 22:46:50 -0400

    first

--- main.tf
+++ main.tf
@@ -13,19 +13,5 @@ resource "aws_vpc" "actions" {
   }
 }
 
-terraform {
-  required_version = "~> 1.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0" # Optional but recommended in production
-    }
-  }
-  backend "s3" {
-    bucket = "class-magnus-amudi"
-    key = "prod/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
 
 
