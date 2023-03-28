1f85873d7fcdcf3ac292b535719ab3049d905111
magamudi <118616073+magamudi@users.noreply.github.com>
2023-03-27 22:42:34 -0400

    3rd commit

---
+++ .github/main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "actions" {
+   cidr_block = "10.0.0.0/24"
+
+  tags = {
+    Name = "Class30"
+    Team = "DevOps"
+    Environment = "Prod"
+    Division = "HR"
+  }
+}
+
+terraform {
+  required_version = "~> 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0" # Optional but recommended in production
+    }
+  }
+  backend "s3" {
+    bucket = "class-magnus-amudi"
+    key = "prod/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
