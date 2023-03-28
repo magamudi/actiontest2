cd9bdef95f42a27faf705d0d5320cd6d95dbddc5
magamudi <118616073+magamudi@users.noreply.github.com>
2023-03-27 23:08:11 -0400

    first

--- main.tf
+++ main.tf
@@ -1,16 +1,9 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.74.0"
-    }
-  }
-}
 
+
+# Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
 }
-
 resource "aws_vpc" "actions" {
    cidr_block = "10.0.0.0/24"
 
@@ -22,5 +15,19 @@ resource "aws_vpc" "actions" {
   }
 }
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+ backend "s3" {
+    bucket = "class-magnus-amudi"
+    key = "prod/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
 
 
